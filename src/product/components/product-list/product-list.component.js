d75dd6a2501dc7296848299aea04af600e11c741
Jinto Jose <jintoppy@gmail.com>
2018-02-14 00:09:07 -0800

    product list http

--- src/product/components/product-list/product-list.component.js
+++ src/product/components/product-list/product-list.component.js
@@ -1,4 +1,16 @@
 angular.module('shoppingcart.product')
+    .controller('ProductListCtrl', ['ProductService', 
+    function(ProductService){
+        var self = this;
+        self.products = [];
+        self.$onInit = function(){
+            ProductService.getProducts()
+                .then(function(result){
+                    self.products = result.data;       
+                });
+        };
+    }])
     .component('productList', {
-        templateUrl: 'src/product/components/product-list/product-list.component.html'
+        templateUrl: 'src/product/components/product-list/product-list.component.html',
+        controller: 'ProductListCtrl'
     });
