f592c8ae83b91db53f937d83910a9ba6b79f0b13
Jinto Jose <jintoppy@gmail.com>
2018-02-14 02:21:41 -0800

    form ngmodel

--- src/product/components/add-product/add-product.component.js
+++ src/product/components/add-product/add-product.component.js
@@ -1,4 +1,8 @@
 angular.module('shoppingcart.product')
+    .controller('AddProductCtrl', [function(){
+        this.newProduct = {};
+    }])
     .component('addProduct', {
-        templateUrl: 'src/product/components/add-product/add-product.component.html'
+        templateUrl: 'src/product/components/add-product/add-product.component.html',
+        controller:'AddProductCtrl'
     });
