2e28b23a13ca32d5b5f1d0bc00837d3405c821b6
Jinto Jose <jintoppy@gmail.com>
2018-02-13 23:42:33 -0800

    styles

--- src/app.module.js
+++ src/app.module.js
@@ -1,5 +1,31 @@
 angular.module('shoppingcart', [
+    'ui.router',
     'shoppingcart.shared',
     'shoppingcart.product',
     'shoppingcart.cart' 
 ]);
+
+angular.module('shoppingcart')
+    .config(['$stateProvider', '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider.state({
+            name: 'home',
+            url: '/',
+            component: 'productList'
+        });
+
+        $stateProvider.state({
+            name: 'addProduct',
+            url: '/add-product',
+            component: 'addProduct'
+        });
+
+        $stateProvider.state({
+            name: 'cart',
+            url: '/cart',
+            component: 'cart'
+        });
+
+        $urlRouterProvider.otherwise('/');
+
+    }]);
